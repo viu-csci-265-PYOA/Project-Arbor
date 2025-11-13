d52663e927ffed00c7b00812378bcdabb9d6995c
milutopaz <milutopaz@gmail.com>
2025-11-12 17:20:19 -0800

    created item and inventory class.

---
+++ src/inventory.cpp
@@ -0,0 +1,54 @@
+#include "inventory.hpp"
+
+Inventory::~Inventory() {
+    for(auto i: items) {
+        delete i;
+    }
+}
+
+void Inventory::add_item(Item* new_item) {
+    items.emplace_back(new_item);
+}
+
+int Inventory::name_search(std::string target_name) {
+    for(int i = 0; i < items.size(); i++) {
+        if(items[i]->get_name() == target_name) {
+            return i;
+        }
+    }
+    //in the case no match is found;
+    return -1;
+}
+
+int Inventory::search(Item* target_item) {
+    for(int i = 0; i < items.size(); i++) {
+        if(items[i] == target_item) {
+            return i;
+        }
+    }
+    //in the case no match is found;
+    return -1;
+}
+//overload for string
+bool Inventory::check_has_item(std::string target_name) {
+    int result = name_search(target_name);
+    if(result != -1) {return true;} else {return false;}
+}
+
+//overload for item
+bool Inventory::check_has_item(Item* target_item) {
+    int result = search(target_item);
+    if(result != -1) {return true;} else {return false;}
+}
+
+int Inventory::get_inventory_count() const {
+    return items.size();
+}
+
+std::vector<std::string> Inventory::get_item_list() const {
+    std::vector<std::string> item_list;
+    for(int i = 0; i < items.size(); i++) {
+        item_list[i] = items[i]->get_name();
+    }
+    return item_list;
+}
---
+++ src/inventory.hpp
@@ -0,0 +1,45 @@
+#ifndef ARBOR_SRC_ITEM_H
+#define ARBOR_SRC_ITEM_H
+
+#include <iostream>
+#include <vector>
+
+class Item {
+private:
+    std::string name;
+    std::string desc;
+public:
+    Item(std::string item_name, std::string item_desc) 
+        : name(item_name), desc(item_desc) {}
+
+    std::string get_name() const {return name;}
+    std::string get_desc() const {return desc;}
+
+    void set_name(std::string item_name) {name = item_name;}
+    void set_desc(std::string item_desc) {desc = item_desc;}
+
+};
+
+//vector of items to be associated with a Character.
+class Inventory {
+private:
+    std::vector<Item*> items;
+
+    int name_search(std::string target_name);
+    int search(Item* target_item);
+public:
+    Inventory() {}
+    Inventory(Item* first_item) {items.emplace_back(first_item);}
+
+    ~Inventory();
+
+    int get_inventory_count() const {return items.size();}
+    std::vector<std::string> get_item_list() const;
+
+    void add_item(Item* new_item);
+
+    bool check_has_item(std::string target_name);
+    bool check_has_item(Item* target_item);
+};
+
+#endif
